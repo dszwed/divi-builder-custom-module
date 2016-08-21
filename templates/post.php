e2ed103a996656a84eeae269f75889dce208df15
Dawid Szwed <dawid@gootek.pl>
2016-08-21 22:58:03 +0200

    Fix conflict with ob_start

--- templates/post.php
+++
@@ -1,9 +0,0 @@
-<div class="<?php post_class(); ?>">
-    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php if($show_meta && $show_date): ?>
-    <?php echo date_i18n( 'd F Y'); ?>
-    <?php endif; ?>
-    <?php if($show_excerpt): ?>
-        <?php the_excerpt();?>
-    <?php endif; ?>
-</div>
